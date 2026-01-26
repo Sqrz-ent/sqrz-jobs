15be9f9a69edb249f256be166d153bd70e96c488
patrick wilhelm <patrickwilhelm@SQRZ-Air.local>
2026-01-26 18:57:08 +0100

    routing

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route(":company/:position", "routes/job-detail.tsx"),
+  route("og/job", "routes/og.job.tsx"),
 ] satisfies RouteConfig;
