085a2f7bafb8b0b7771a8bcc0ce77a83b0801d47
patrick wilhelm <patrickwilhelm@SQRZ-Air.local>
2026-01-26 21:24:13 +0100

    job list

--- app/routes/jobs.tsx
+++ app/routes/jobs.tsx
@@ -1,8 +1,5 @@
-export default function Jobs() {
-  return (
-    <main style={{ padding: 32 }}>
-      <h1>Jobs</h1>
-      <p>List will go here</p>
-    </main>
-  );
+import { Outlet } from "react-router";
+
+export default function JobsLayout() {
+  return <Outlet />;
 }
